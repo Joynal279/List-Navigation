631ea8e3c5bfc3b3b5d91591e20e20cca22a081e
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-24 21:26:27 +0600

    Initial Commit

---
+++ List&Navigation/List_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_NavigationApp.swift
+//  List&Navigation
+//
+//  Created by JOYNAL ABEDIN on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct List_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
