b0940fe22220ec95facee8590819271e2335fc51
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-25 18:41:41 +0600

    toDoItem model added

--- List&Navigation/ContentView.swift
+++ List&Navigation/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var toggleStatus = true
+    
+    var listData: [ToDoItem] = [
+        ToDoItem(task: "Take out trash", imageName: "trash.circle.fill"),
+        ToDoItem(task: "Pick up the kids", imageName: "person.2.fill"),
+        ToDoItem(task: "Wash the car", imageName: "car.fill")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                Section(header: Text("Settings")) {
+                    Toggle(isOn: $toggleStatus) {
+                        Text("Allow Notification")
+                    }
+                    
+                }
+                Section(header: Text("To Do Task")) {
+                    ForEach(listData) { item in
+                    NavigationLink(destination: Text(item.task)) {
+                            HStack {
+                                Image(systemName: item.imageName)
+                                Text(item.task)
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItem)
+                    .onMove(perform: moveItem)
+                    
+                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarItems(trailing: EditButton())
+//            .navigationBarItems(trailing: Button(action: addTask, label: {
+//                Text("Add")
+//            }))
         }
-        .padding()
+    }
+    
+    func deleteItem(at offsets: IndexSet){
+        //delete item from data source here
+    }
+    func moveItem(from source: IndexSet, to destination: Int) {
+        //Reorder items is source data here
     }
 }
 
---
+++ List&Navigation/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  List&Navigation
+//
+//  Created by JOYNAL ABEDIN on 24/9/22.
+//
+
+import Foundation
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var task: String
+    var imageName: String
+}
